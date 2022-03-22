6ece15363e456774aa35bed3740326ed50d6c7c1
Luiz Anacleto <luizfernandoanacleto68@gmail.com>
2022-03-22 19:51:54 -0300

    fix problem with attributeRemover

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const attributesRemover = require('./src/controller/attributesRemover');
+const selectableleAttributesRemover = require('./src/controller/selectableleAttributesRemover');
 
 const app = express();
 
 app.use(express.json({ type: '*/*' }));
 
 app.get('/attributesRemover', attributesRemover);
+app.get('/selectableleAttributesRemover', selectableleAttributesRemover);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
--- src/controller/attributesRemover.js
+++ src/controller/attributesRemover.js
@@ -1,22 +1,27 @@
 const { JSDOM } = require('jsdom');
 
-function attributesRemover(req, res) {
-  const { html } = req.body
+function getAttributes(element) {
+	const arr = Array();
+	for (let i = 0; i < element.attributes.length; i++) {
+		arr.push(element.attributes[i].nodeName);
+	}
+  return arr;
+}
 
-  if (!html) res.status(400).json({ error: 'Missing html' });
-  if (html.includes('<body' || html.includes('<head'))) res.status(400).json({ error: "Don't send body and head tags" });
+function attributesRemover(req, res) {
+  const { html } = req.body;
+
+  if (!html) return res.status(400).json({ error: 'Missing html' });
+  if (html.includes('<body' || html.includes('<head'))) return res.status(400).json({ error: "Don't send body and head tags" });
 
   const dom = new JSDOM(`<body>${html}</body>`);
   dom.window.document.querySelectorAll('*').forEach((el) => {
-    const attributes = Object.fromEntries(Array.from(el.attributes)
-      .map((item) => [item.name, item.value]));
-    if (Object.keys(attributes).length === 0) return;
-    Object.entries(attributes).forEach((atribute) => {
-      el.removeAttribute(atribute);
+    getAttributes(el).forEach((attr) => {
+      el.removeAttribute(attr);
     });
   });
 
-  res.status(200).json({
+  return res.status(200).json({
     html: dom.window.document.body.innerHTML,
   });
 }
